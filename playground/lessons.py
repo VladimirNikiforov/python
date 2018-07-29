3284b09123dba4de91173b74f02048c57fdf2f6e
Vladimir <developer.vn.d@gmail.com>
2018-07-29 18:23:37 +0300

    Added sets

--- playground/lessons.py
+++ playground/lessons.py
@@ -180,3 +180,51 @@ for number in range(10):
 
 for key in ordered:
     print(key)
+
+#######################
+# Sets
+
+empty_set = set()
+number_set = {1, 2, 3, 3, 4, 5}
+print(number_set)
+print(2 in number_set)
+
+odd_set = set()
+even_set = set()
+for number in range(10):
+    if number % 2:
+        odd_set.add(number)
+    else:
+        even_set.add(number)
+
+print(odd_set)
+print(even_set)
+
+# Union of sets
+union_set = odd_set | even_set
+union_set = odd_set.union(even_set)
+print(union_set)
+
+# Intersection of sets
+intersection_set = odd_set & even_set
+intersection_set = odd_set.intersection(even_set)
+print(intersection_set)
+
+# difference between sets
+difference_set = odd_set - even_set
+difference_set = odd_set.difference(even_set)
+print(difference_set)
+
+# symmetric difference between sets
+symmetric_difference_set = odd_set ^ even_set
+symmetric_difference_set = odd_set.symmetric_difference(even_set)
+print(symmetric_difference_set)
+
+even_set.remove(2)
+print(even_set)
+
+print(even_set.pop())
+print(even_set)
+
+frozen = frozenset(['A', 'B', 'C'])  # immutable!
+# frozen.add('O') # Error!
