94beaea0fdc9abc43cd6e3e4648c426d90166408
Vladimir <developer.vn.d@gmail.com>
2018-07-31 20:10:24 +0300

    Tutor solution for second task added

--- tasks_and_solutions/week02_02.py
+++ tasks_and_solutions/week02_02.py
@@ -19,3 +19,19 @@ def get_data():
     }
 print(get_data())  # вернёт '{"data": 42}'
 """
+
+""" # tutors solution
+
+import functools
+import json
+
+
+def to_json(func):
+
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return json.dumps(result)
+
+    return wrapped
+"""
