b5cc478daa4162d43a7aab37ca2074d607dfb2b9
Vladimir <developer.vn.d@gmail.com>
2018-07-31 18:23:54 +0300

    Decorators added

--- playground/lessons.py
+++ playground/lessons.py
@@ -448,10 +448,121 @@ def week_2():
         # print({num % 10 for num in range(100)})
         num_list = range(7)
         squared_list = [x ** 2 for x in num_list]
-        # print(list(zip(num_list, squared_list)))
+        print(list(zip(num_list, squared_list)))
         # [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36)]
 
-    functional_programming()
+    def decorators():
+        """
+        def decorator(func):
+            return func
+        @decorator
+        def decorated():
+            print('Hello!')
+        decorated = decorator(decorated)
+        """
+        """
+        def decorator(func):
+            def new_func():
+                pass
+            return new_func
+
+        @decorator
+        def decorated():
+            print('Hello!')
+        decorated()
+        print(decorated.__name__)
+        """
+        """
+        # Need decorator that writes result of decorated function to log
+        
+        import functools
+        def logger(func):
+            @functools.wraps(func)
+            def wrapped(*args, **kwargs):
+                result = func(*args, **kwargs)
+                with open('log.txt','w') as f:
+                    f.write(str(result))
+
+                return result
+            return wrapped
+
+        @logger
+        def summator(num_list):
+            return sum(num_list)
+        print('Summator: {}'.format(summator([1, 2, 3, 4, 5])))
+        with open('log.txt', 'r') as f:
+            print('log.txt: {}'.format(f.read()))
+        print(summator.__name__)
+        """
+        """
+        # Need decorator with param, which writes log to specified file
+        
+        def logger(filename):
+            def decorator(func):
+                def wrapped(*args, **kwargs):
+                    result = func(*args, **kwargs)
+                    with open(filename, 'w') as f:
+                        f.write(str(result))
+                    return result
+                return wrapped
+            return decorator
+
+        @logger('new_log.txt')
+        def summator(num_list):
+            return sum(num_list)
+
+        print('Summator: {}'.format(summator([1, 2, 3, 4, 5, 6])))
+        with open('new_log.txt', 'r') as f:
+            print('new_log.txt: {}'.format(f.read()))
+        """
+
+        """
+        If we use many decorators?
+        
+        def first_decorator(func):
+            def wrapped():
+                print('Inside first_decorator product')
+                return func()
+            return wrapped
+        def second_decorator(func):
+            def wrapped():
+                print('Inside second_decorator product')
+                return func()
+            return wrapped
+
+        @first_decorator
+        @second_decorator
+        def decorated():
+            print('Finally called...')
+        # decorated = first_decorator(second_decorator(decorated))
+        decorated()
+        #Inside first_decorator product
+        #Inside second_decorator product
+        #Finally called...
+        """
+
+        def bold(func):
+            def wrapped():
+                return "<b>" + func() + "</b>"
+
+            return wrapped
+
+        def italic(func):
+            def wrapped():
+                return "<i>" + func() + "</i>"
+
+            return wrapped
+
+        @bold
+        @italic
+        def hello():
+            return "hello world"
+
+        # hello = bold(italic(hello))
+        print(hello())
+        # <b><i>hello world</i></b>
+
+    decorators()
 
 
 week_2()
