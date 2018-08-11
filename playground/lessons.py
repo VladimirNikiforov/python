733d2be2ae812ad4a0727eacf226a0400fba4982
Vladimir <developer.vn.d@gmail.com>
2018-08-11 18:22:14 +0300

    waw

--- playground/lessons.py
+++ playground/lessons.py
@@ -932,4 +932,16 @@ def week_3():
         def __init__(self, name=None):
             self.name = name
 
+    class Dog(Pet):
+        def __init__(self, name, breed=None):
+            super().__init__(name)
+            self.breed = breed
+
+        def say(self):
+            return "{0}: waw".format(self.name)
+
+    dog = Dog("Шарик", "Доберман")
+    print(dog.name)
+    print(dog.say())
+
 week_3()
