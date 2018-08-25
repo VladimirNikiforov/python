a3b71eab3ff987f7cc62a78008f82508973948fe
Vladimir <developer.vn.d@gmail.com>
2018-08-25 16:57:20 +0300

    Property

--- playground/lessons.py
+++ playground/lessons.py
@@ -1683,7 +1683,35 @@ def week_4():
         # В bound метод по умолчанию передаётся объект, с которым вызван метод.
         # И именно он и записывается в атрибут self метода класса.
 
+        class Property:
+            def __init__(self, getter):
+                self.getter = getter
 
+            def __get__(self, obj, obj_type=None):
+                if obj is None:
+                    return self
+
+                return self.getter(obj)
+
+        class Class:
+            @property
+            def original(self):
+                return 'original'
+
+            @Property
+            def custom_sugar(self):
+                return 'custom sugar'
+
+            def custom_pure(self):
+                return 'custom pure'
+
+            custom_pure = Property(custom_pure)
+
+        obj = Class()
+
+        print(obj.original)
+        print(obj.custom_sugar)
+        print(obj.custom_pure)
 
     descriptors()
 week_4()
