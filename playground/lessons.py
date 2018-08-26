4fcc3dbce046f408c7c3118915c80b393e9e5be5
Vladimir <developer.vn.d@gmail.com>
2018-08-26 11:08:45 +0300

    debugging with pdb

--- playground/lessons.py
+++ playground/lessons.py
@@ -1814,5 +1814,30 @@ def week_4():
         # https://docs.python.org/3/howto/descriptor.html
     # metaclasses()
 
+    def debugging():
+        import requests
+        import re
 
+        def main(site_url, substring):
+            # import pdb
+            # pdb.set_trace()
+
+            site_code = get_site_code(site_url)
+            matching_substrings = get_matching_substrings(site_code, substring)
+            print(f'"{substring}" found {len(matching_substrings)} times in {site_url}')
+
+        def get_site_code(site_url):
+            if not site_url.startswith('http'):
+                site_url = 'http://' + site_url
+
+            return requests.get(site_url).text
+
+        def get_matching_substrings(source, substring):
+            return re.findall(substring, source)
+
+        main('mail.ru', 'script')  # "script" found 283 times in mail.ru
+        # run from console with debugging:
+        # python -m pdb lessons.py
+
+    debugging()
 week_4()
