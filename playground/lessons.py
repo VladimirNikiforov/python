e983eaff38fead288215970cb0ecfe9ef3bf750b
Vladimir <developer.vn.d@gmail.com>
2018-09-05 19:05:53 +0300

    using_blocks

--- playground/lessons.py
+++ playground/lessons.py
@@ -2121,6 +2121,26 @@ def week5():
         th1.join();
         th2.join()
 
-    using_queue()
+    # using_queue()
+
+    def using_blocks():
+        import threading
+
+        class Point(object):
+            def __init__(self):
+                self._mutex = threading.RLock()
+                self._x = 0
+                self._y = 0
+
+            def get(self):
+                with self._mutex:
+                    return (self._x, self._y)
+
+            def set(self, x, y):
+                with self._mutex:
+                    self._x = x
+                    self._y = y
+
+    using_blocks()
 
 week5()
