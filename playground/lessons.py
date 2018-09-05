42637e7d2be8f9e434ddcf8fb1ea56550b6c80df
Vladimir <developer.vn.d@gmail.com>
2018-09-05 18:45:50 +0300

    using ThreadPoolExecutor

--- playground/lessons.py
+++ playground/lessons.py
@@ -2079,6 +2079,21 @@ def week5():
         th.start()
         th.join()
 
-    simple_thread2()
+    # simple_thread2()
+
+    def using_threadpoolexecutor():
+        from concurrent.futures import ThreadPoolExecutor, as_completed
+
+        def f(a):
+            return a * a
+
+        # .shutdown() in exit
+        with ThreadPoolExecutor(max_workers=3) as pool:
+            results = [pool.submit(f, i) for i in range(10)]
+
+            for future in as_completed(results):
+                print(future.result())
+
+    using_threadpoolexecutor()
 
 week5()
