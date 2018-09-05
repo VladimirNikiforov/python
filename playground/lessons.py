e5a719fddb2da6d3da01b5b06794afeed7b05c63
Vladimir <developer.vn.d@gmail.com>
2018-09-05 19:16:09 +0300

    using_conditions

--- playground/lessons.py
+++ playground/lessons.py
@@ -2157,6 +2157,36 @@ def week5():
                 a.release()
                 b.release()
 
-    using_blocks2()
+    # using_blocks2()
+
+    def using_conditions():
+        import threading
+
+        class Queue(object):
+            def __init__(self, size=5):
+                self._size = size
+                self._queue = []
+                self._mutex = threading.RLock()
+                self._empty = threading.Condition(self._mutex)
+                self._full = threading.Condition(self._mutex)
+
+            def put(self, val):
+                with self._full:
+                    while len(self._queue) >= self._size:
+                        self._full.wait()
+
+                    self._queue.append(val)
+                    self._empty.notify()
+
+            def get(self):
+                with self._empty:
+                    while len(self._queue) == 0:
+                        self._empty.wait()
+
+                    ret = self._queue.pop(0)
+                    self._full.notify()
+                    return ret
+
+    using_conditions()
 
 week5()
