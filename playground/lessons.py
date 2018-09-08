620eb7f078bc3e3d361da8ba263c5c260d29cbbc
Vladimir <developer.vn.d@gmail.com>
2018-09-08 11:46:21 +0300

    socket_with_context_manager

--- playground/lessons.py
+++ playground/lessons.py
@@ -2260,6 +2260,31 @@ def week5():
         sock.sendall("ping".encode("utf8"))
         sock.close()
 
-    client_side_socket()
+    # client_side_socket()
+
+    def socket_with_context_manager():
+        # server
+        import socket
+
+        with socket.socket() as sock:
+            sock.bind(("", 10001))
+            sock.listen()
+
+            while True:
+                conn, addr = sock.accept()
+                with conn:
+                    while True:
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        print(data.decode("utf8"))
+
+        # clent
+        import socket
+
+        with socket.create_connection(("127.0.0.1", 10001)) as sock:
+            sock.sendall("ping".encode("utf8"))
+
+    socket_with_context_manager()
 
 week5()
