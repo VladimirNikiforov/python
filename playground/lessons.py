c6fefb3a726731509c58eb00ab64030a96ab9ba8
Vladimir <developer.vn.d@gmail.com>
2018-09-29 13:47:26 +0300

    several_requests_by_processes_and_threads_server

--- playground/lessons.py
+++ playground/lessons.py
@@ -2365,6 +2365,22 @@ def week5():
                         print(data.decode("utf8"))
 
     def several_requests_by_processes_and_threads_server():
+        def process_request(conn, addr):
+            print("connected client:", addr)
+            with conn:
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print(data.decode("utf8"))
+
+        def worker(sock):
+            while True:
+                conn, addr = sock.accept()
+                print("pid", os.getpid())
+                th = threading.Thread(target=process_request, args=(conn, addr))
+                th.start()
+
         import socket
         import multiprocess
 
@@ -2381,22 +2397,6 @@ def week5():
 
             for w in workers_list:
                 w.join()
-
-        def process_request(conn, addr):
-            print("connected client:", addr)
-            with conn:
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    print(data.decode("utf8"))
-
-        def worker(sock):
-            while True:
-                conn, addr = sock.accept()
-                print("pid", os.getpid())
-                th = threading.Thread(target=process_request, args=(conn, addr))
-                th.start()
 
 
 
