57b173ddf3fc04561757701f25c1fb4119a8771e
Vladimir <developer.vn.d@gmail.com>
2018-09-30 16:30:23 +0300

    pep380_generators

--- playground/lessons.py
+++ playground/lessons.py
@@ -2566,5 +2566,29 @@ def week5():
         
         """
 
+    def pep380_generators():
+        def chain(x_iterable, y_iterable):
+            yield from x_iterable
+            yield from y_iterable
+
+        def the_same_chain(x_iterable, y_iterable):
+            for x in x_iterable:
+                yield x
+
+            for y in y_iterable:
+                yield y
+
+        """    
+        >>> a = [1, 2, 3]
+        >>> b = (4,5)
+        >>> for x in chain(a, b):
+                print(x)
+        1
+        2
+        3
+        4
+        5
+        """
+
 
 week5()
