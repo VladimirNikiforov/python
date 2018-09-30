52eff378afa8c17092da3f7c07e4617fdc3ba5f2
Vladimir <developer.vn.d@gmail.com>
2018-09-30 16:15:35 +0300

    corutines_exit

--- playground/lessons.py
+++ playground/lessons.py
@@ -2509,4 +2509,27 @@ def week5():
         Сопрограмма содержит инструкцию yield и ожидает пока кто-то отправит в нее значение при помощи метода send
         """
 
+    def corutines_exit():
+        def grep(pattern):
+            print("start grep")
+            try:
+                while True:
+                    line = yield
+                    if pattern in line:
+                        print(line)
+            except GeneratorExit:
+                print("stop grep")
+
+    """
+        >> g = grep("python")
+        >> next(g) # g.send(None)
+        start grep
+        >> g.send("python is the best!")
+        python is the best!
+        >> g.close()
+        stop grep
+    """
+
+
+
 week5()
