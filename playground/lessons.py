5d57d99a41bdef7fa0df5ef38d37efc12c1a4bbf
Vladimir <developer.vn.d@gmail.com>
2018-10-06 15:24:50 +0300

    use_asyncio_def

--- playground/lessons.py
+++ playground/lessons.py
@@ -2603,5 +2603,20 @@ def week5():
         loop.run_until_complete(hello_world())
         loop.close()
 
+    def use_asyncio_def():
+        # asyncio, async def / await; PEP 492 Python3.5
+        import asyncio
+
+        asyncio
+
+        def hello_world():
+            while True:
+                print("Hello World!")
+                await asyncio.sleep(1.0)
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(hello_world())
+        loop.close()
+
 
 week5()
