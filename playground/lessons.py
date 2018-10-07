58c26918bbefbdeacd98b769c5a2fc5cd5b31f6d
Vladimir <developer.vn.d@gmail.com>
2018-10-07 15:58:33 +0300

    asyncio_task

--- playground/lessons.py
+++ playground/lessons.py
@@ -2705,11 +2705,12 @@ def week5():
 
         task_list = [loop.create_task(sleep_task(i)) for i in range(2)]
         loop.run_until_complete(asyncio.wait(task_list))
-
+        """
         loop.run_until_complete(loop.create_task(sleep_task(3)))
         loop.run_until_complete(asyncio.gather(
             sleep_task(10),
             sleep_task(20),
         ))
+        """
 
 week5()
